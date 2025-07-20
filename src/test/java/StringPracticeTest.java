3e55ece91e9735b862f3c5155227c112021ba50f
Mohanasundaram <mohanasundaram.vlr@gmail.com>
2025-07-20 20:31:09 +0800

    Added jenkins configuration

--- src/test/java/StringPracticeTest.java
+++ src/test/java/StringPracticeTest.java
@@ -24,4 +24,29 @@ public class StringPracticeTest {
         assertNull(result);
     }
 
+    @Test
+    void testGetReverseName_basic() {
+        assertEquals("nhoJ", StringPractice.getReverseName("John"));
+    }
+
+    @Test
+    void testGetReverseName_singleCharacter() {
+        assertEquals("A", StringPractice.getReverseName("A"));
+    }
+
+    @Test
+    void testGetReverseName_emptyString() {
+        assertEquals("", StringPractice.getReverseName(""));
+    }
+
+    @Test
+    void testGetReverseName_palindrome() {
+        assertEquals("madam", StringPractice.getReverseName("madam"));
+    }
+
+    @Test
+    void testGetReverseName_withSpaces() {
+        assertEquals("dlroW olleH", StringPractice.getReverseName("Hello World"));
+    }
+
 }
